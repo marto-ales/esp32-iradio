da6b26e343f0e8ff1b3d309ccb88773822ef578e
Martin Ales <martin@gcoop.coop>
2023-09-30 00:36:24 -0300

    Refactor using more appropiate classes and working meta data

--- IRadio.ino
+++ IRadio.ino
@@ -4,10 +4,9 @@
 #include <LiquidCrystal_I2C.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "WiFi.h"
 
 TaskHandle_t playerTaskHandle = NULL;
- 
+
 LiquidCrystal_I2C lcd(0x27,16,2);  // set the LCD address to 0x27 for a 16 chars and 2 line display
 
 
@@ -30,12 +29,15 @@ const char *titles[] = {
   "Internacional 3"
 };
 
-URLStreamBuffered urlStream(ssid, password);
-AudioSourceURL source(urlStream, urls, "audio/mp3");
-AnalogAudioStream out; 
-AudioInfo info(44100, 2, 16);
-MP3DecoderHelix decoder;
-AudioPlayer player(source, out, decoder);
+//URLStreamBuffered urlStream(ssid, password);
+URLStreamBuffered urlStream(ssid, password, 1024);
+//AudioSourceURL source(urlStream, urls, "audio/mp3");
+MetaDataOutput out1;
+AnalogAudioStream dac;
+EncodedAudioStream out2dec(&dac, new MP3DecoderHelix()); // Decoding stream
+MultiOutput out;
+StreamCopy copier(out, urlStream); // copy url to decoder
+
  
 int station_index = 0;
 int station_count = sizeof(urls) / sizeof(urls[0]);
@@ -79,7 +81,7 @@ void readRotary() {
     station_index = rotVal;
     Serial.println(urls[station_index]);
     Serial.println(titles[station_index]);
-    lcd_text(titles[station_index]);
+    lcd_text(titles[station_index], NULL);
   }
   clkLast = clkState;
 
@@ -95,26 +97,34 @@ void readRotary() {
   delay(1);
 }
 
-void callbackPrintMetaData(MetaDataType type, const char* str, int len) {
+void printMetaData(MetaDataType type, const char* str, int len) {
   Serial.print("==> ");
   Serial.print(toStr(type));
   Serial.print(": ");
   Serial.println(str);
+  if (type == 4) {
+    lcd_text(titles[station_index], str);
+  }
+  
 }
 
 void ChangeRadio() {
-  player.stop();
+  //copier.end();
   vTaskSuspend(playerTaskHandle);
-  source.selectStream(station_index);
-  player.setAudioSource(source);
-  player.play();
+  urlStream.end();
+  urlStream.begin(urls[station_index], "audio/mp3");
+  out1.end();
+  out1.setCallback(printMetaData);
+  out1.begin(urlStream.httpRequest());
+  //player.setAudioSource(source);
+  //copier.begin();
   vTaskResume(playerTaskHandle);
 
 }
 
 void Player_task(void *arg) {
   while(1){
-    player.copy();
+    copier.copy();
   }
 }
 
@@ -126,36 +136,56 @@ void setup() {
 
   //Serial
   Serial.begin(115200);
+  AudioLogger::instance().begin(Serial, AudioLogger::Warning);
+
 
   lcd.init();                      // initialize the lcd
   lcd.backlight();
 
+  // setup multi output
+  out.add(out1);
+  out.add(out2dec);
+
+  urlStream.begin(urls[0],"audio/mp3");
+    // setup metadata
+  out1.setCallback(printMetaData);
+  out1.begin(urlStream.httpRequest());
+  // setup player
+  //urlStream.setMetadataCallback(callbackPrintMetaData);
+// mp3 radio
+  //urlStream.begin();
+
 
   // setup output
-  auto config = out.defaultConfig(TX_MODE);
-  config.copyFrom(info); 
-  out.begin(config);
+  auto config = dac.defaultConfig(TX_MODE);
+  dac.begin(config);
 
 
-  // setup player
-  source.setMetadataCallback(callbackPrintMetaData);
-  player.begin();
-  lcd_text("IRadio");
+
+  // setup I2S based on sampling rate provided by decoder
+  out2dec.setNotifyAudioChange(dac);
+  out2dec.begin();
+
+
+
   // start on core 0
-  xTaskCreate(Player_task, "Player_Task", 4096, NULL, 10, &playerTaskHandle);
-    
+  xTaskCreatePinnedToCore(Player_task, "Player_Task", 4096, NULL, 10, &playerTaskHandle,0);
+  
 }
 
- 
+
 void loop() {
-  readRotary();
-   
+  while(1) {
+    readRotary();
+  }
 }
  
  
-void lcd_text(String text) {
+void lcd_text(const char* line1, const char* line2) {
   lcd.clear();
   lcd.setCursor(0, 0);             // Start at top-left corner
-  lcd.print(text);
+  lcd.print(line1);
+  lcd.setCursor(0, 1);             // Start at top-left corner
+  lcd.print(line2);
 }
 
