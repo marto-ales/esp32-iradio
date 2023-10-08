5f70ad43bf585d16bb0cd57029ce8b1134a91f58
Martin Ales <martin@gcoop.coop>
2023-10-08 17:01:37 -0300

    WIP: Implement configuration web server

--- IRadio.ino
+++ IRadio.ino
@@ -61,6 +61,15 @@ int clkLast;
 int swState  = HIGH;
 int swLast  = HIGH;
 
+WiFiServer server(80);
+// Current time
+unsigned long currentTime = 0;
+// Previous time
+unsigned long previousTime = 0;
+// Define timeout time in milliseconds (example: 2000ms = 2s)
+const long timeoutTime = 2000;
+// Variable to store the HTTP request
+String header;
 
 void readRotary() {
    /* function readRotary */
@@ -175,8 +184,7 @@ void setup() {
   lcd.backlight();
 
   wifi_setup();
-  
-
+  server.begin();
   // setup multi output
   out.add(out1);
   out.add(out2dec);
@@ -186,19 +194,13 @@ void setup() {
   out1.setCallback(printMetaData);
   out1.begin(urlStream.httpRequest());
 
-
-
   // setup output
   auto config = dac.defaultConfig(TX_MODE);
   dac.begin(config);
 
-
-
   // setup I2S based on sampling rate provided by decoder
   out2dec.setNotifyAudioChange(dac);
   out2dec.begin();
-
-
 
   // start on core 0
   xTaskCreatePinnedToCore(Player_task, "Player_Task", 4096, NULL, 10, &playerTaskHandle,0);
@@ -209,6 +211,73 @@ void setup() {
 void loop() {
   readRotary();
   waitAndShowCurrent();
+  webServer();
+}
+
+void webServer() {
+  WiFiClient client = server.available();   // Listen for incoming clients
+
+  if (client) {                             // If a new client connects,
+    currentTime = millis();
+    previousTime = currentTime;
+    Serial.println("New Client.");          // print a message out in the serial port
+    String currentLine = "";                // make a String to hold incoming data from the client
+    while (client.connected() && currentTime - previousTime <= timeoutTime) {  // loop while the client's connected
+      currentTime = millis();
+      if (client.available()) {             // if there's bytes to read from the client,
+        char c = client.read();             // read a byte, then
+        Serial.write(c);                    // print it out the serial monitor
+        header += c;
+        if (c == '\n') {                    // if the byte is a newline character
+          // if the current line is blank, you got two newline characters in a row.
+          // that's the end of the client HTTP request, so send a response:
+          if (currentLine.length() == 0) {
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+            // and a content-type so the client knows what's coming, then a blank line:
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println("Connection: close");
+            client.println();
+
+            // Display the HTML web page
+            client.println("<!DOCTYPE html><html>");
+            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+            client.println("<link rel=\"icon\" href=\"data:,\">");
+            // CSS to style the on/off buttons
+            // Feel free to change the background-color and font-size attributes to fit your preferences
+            client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
+            client.println(".button { background-color: #4CAF50; border: none; color: white; padding: 16px 40px;");
+            client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
+            client.println(".button2 {background-color: #555555;}</style></head>");
+
+            // Web Page Heading
+            client.println("<body><h1>ESP32 IRadio</h1>");
+
+            // Display current state, and ON/OFF buttons for GPIO 26
+            client.println("<p>GPIO 26 - State </p>");
+            // If the output26State is off, it displays the ON button
+            client.println("<p><a href=\"/26/on\"><button class=\"button\">ON</button></a></p>");
+            client.println("</body></html>");
+
+            // The HTTP response ends with another blank line
+            client.println();
+            // Break out of the while loop
+            break;
+          } else { // if you got a newline, then clear currentLine
+            currentLine = "";
+          }
+        } else if (c != '\r') {  // if you got anything else but a carriage return character,
+          currentLine += c;      // add it to the end of the currentLine
+        }
+      }
+    }
+    // Clear the header variable
+    header = "";
+    // Close the connection
+    client.stop();
+    Serial.println("Client disconnected.");
+    Serial.println("");
+  }
 }
 
 void waitAndShowCurrent() {
